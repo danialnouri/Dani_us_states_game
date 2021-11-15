6561e26caa3029ed36c430e8d1e7f47d5e18b281
danialNouri <danial.nouri20@gmail.com>
2021-11-15 23:36:36 +0330

    comprehension lists

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ while len(guessed_states) < 50:
                                     prompt="Whats another states name?").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in states if state not in guessed_states]
+        # for state in states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
         print(missing_states)
         break
     if answer_state in states:
